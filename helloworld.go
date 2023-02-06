1d7e30f8b3986bd4526a8bd676aa266f07d52231
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2023-02-06 14:57:09 +0700

    Data type

---
+++ helloworld.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Num
+	fmt.Println("Satu =", 1)
+	fmt.Println("Dua =", 2)
+	fmt.Println("Tiga koma lima =", 3.5)
+
+	// Bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// String
+	fmt.Println("Raditya Dwisatrio")
+	fmt.Println("Raditya Dwisatrio"[0])   // 82 (byte)
+	fmt.Println(len("Raditya Dwisatrio")) // 17
+}

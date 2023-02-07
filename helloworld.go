f387fc954468117e3386fdd7ac43e8929d831aea
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2023-02-07 09:59:20 +0700

    Finished learn converting data type

--- helloworld.go
+++ helloworld.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
+	var nilai32 int32 = 32678
+	var nilai64 int64 = int64(nilai32) // Success
+	var nilai8 int8 = int8(nilai32)    // Fail - melebihi batas maksimum nilai int8
+
+	fmt.Println(nilai32) // 32678
+	fmt.Println(nilai64) // 32678
+	fmt.Println(nilai8)  // -90
+
+	var name = "Radit"
+	var e = name[0]
+	var eString = string(e)
+
+	fmt.Println(name)    // Radit
+	fmt.Println(e)       // 82
+	fmt.Println(eString) // R
 }
